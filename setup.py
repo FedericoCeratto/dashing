92fe81d8b072d0222322b94b8cc436c658c78c55
Federico Ceratto <federico.ceratto@gmail.com>
2017-06-03 14:02:13 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='dashing',
+      version=version,
+      description="High-level terminal-based dashboard",
+      long_description="""\
+Easily create dashboards""",
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+      ],
+      keywords='dashboard terminal',
+      author='Federico Ceratto',
+      author_email='federico@debian.org',
+      url='https://github.com/FedericoCeratto/dashing',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'blessed'
+      ],
+      )
